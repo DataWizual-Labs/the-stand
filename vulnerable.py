db59551ea06d73c6bb9ee702ccd96b41a8661fcd
DataWizual-Labs <eldorzufarov@yahoo.com>
2026-02-11 22:08:45 +0500

    Delete vulnerable.py

--- vulnerable.py
+++
@@ -1,5 +0,0 @@
-# Test file for Auditor Core
-import os
-password = "AKIAIMNO78987EXAMPLE" # Это должен поймать Gitleaks
-os.system(f"rm -rf {password}")    # Это должен поймать Semgrep/Bandit
-eval("print('hello')")             # Это должен поймать SAST
